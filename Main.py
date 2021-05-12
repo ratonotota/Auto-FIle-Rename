104b714104426c3b52f8cd49d854b541f3391033
edgib102 <58235933+edgib102@users.noreply.github.com>
2021-05-12 19:57:27 +1200

    Added a deleting component

--- Main.py
+++ Main.py
@@ -3,6 +3,15 @@ import os
 print("Started program")
 filePath = "C:\\Users\\edgib102\\Documents\\blender\\saves\\cache_fluid_4b85f272"
 NameList = ["config", "data", "noise"]
+Startframe = 3
+
+def Delete(Files):
+    FileList = []
+    for file in Files:
+        FileList.append(file)
+    for i in range(Startframe-1):
+        os.remove(FileList[i])
+        print("Deleted " + FileList[i].name)
 
 
 def Rename(Files, name, strFiles):
@@ -26,6 +35,7 @@ def CheckBase():
                 print(entry.name + " found")
                 DataFilepath = filePath + "\\" + entry.name
                 print("filepath gotten: " + DataFilepath)
+                Delete(os.scandir(DataFilepath))
                 Rename(os.scandir(DataFilepath), entry.name, DataFilepath)
 
 
